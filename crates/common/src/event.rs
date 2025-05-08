12935501a01dea4d2e99b9ac62ce804b243978cc
storycraft <storycraft@pancake.sh>
2025-05-09 02:29:22 +0900

    feat: remove non_exhaustive

--- crates/common/src/event.rs
+++ crates/common/src/event.rs
@@ -1,13 +1,11 @@
 use bincode::{Decode, Encode};
 
 #[derive(Debug, Encode, Decode, Clone)]
-#[non_exhaustive]
 pub enum ClientEvent {
     Window { hwnd: u32, event: WindowEvent },
 }
 
 #[derive(Debug, Encode, Decode, Clone)]
-#[non_exhaustive]
 pub enum WindowEvent {
     Added,
     Resized { width: u32, height: u32 },
