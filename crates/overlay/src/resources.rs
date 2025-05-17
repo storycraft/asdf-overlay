2b593fc8ac223afa4fdf83c74692452cd92fc24e
storycraft <storycraft@pancake.sh>
2025-05-17 14:11:37 +0900

    fix: rc index starts from 1

--- crates/overlay/src/resources.rs
+++ crates/overlay/src/resources.rs
@@ -2,25 +2,25 @@
 pub mod cursors {
     use windows::core::PCWSTR;
 
-    pub const IDC_ALIAS: PCWSTR = PCWSTR(0 as _);
-    pub const IDC_CELL: PCWSTR = PCWSTR(1 as _);
-    pub const IDC_COLRESIZE: PCWSTR = PCWSTR(2 as _);
-    pub const IDC_COPYCUR: PCWSTR = PCWSTR(3 as _);
-    pub const IDC_HAND_GRAB: PCWSTR = PCWSTR(4 as _);
-    pub const IDC_HAND_GRABBING: PCWSTR = PCWSTR(5 as _);
-    pub const IDC_PAN_EAST: PCWSTR = PCWSTR(6 as _);
-    pub const IDC_PAN_MIDDLE: PCWSTR = PCWSTR(7 as _);
-    pub const IDC_PAN_MIDDLE_HORIZONTAL: PCWSTR = PCWSTR(8 as _);
-    pub const IDC_PAN_MIDDLE_VERTICAL: PCWSTR = PCWSTR(9 as _);
-    pub const IDC_PAN_NORTH: PCWSTR = PCWSTR(10 as _);
-    pub const IDC_PAN_NORTH_EAST: PCWSTR = PCWSTR(11 as _);
-    pub const IDC_PAN_NORTH_WEST: PCWSTR = PCWSTR(12 as _);
-    pub const IDC_PAN_SOUTH: PCWSTR = PCWSTR(13 as _);
-    pub const IDC_PAN_SOUTH_EAST: PCWSTR = PCWSTR(14 as _);
-    pub const IDC_PAN_SOUTH_WEST: PCWSTR = PCWSTR(15 as _);
-    pub const IDC_PAN_WEST: PCWSTR = PCWSTR(16 as _);
-    pub const IDC_ROWRESIZE: PCWSTR = PCWSTR(17 as _);
-    pub const IDC_VERTICALTEXT: PCWSTR = PCWSTR(18 as _);
-    pub const IDC_ZOOMIN: PCWSTR = PCWSTR(19 as _);
-    pub const IDC_ZOOMOUT: PCWSTR = PCWSTR(20 as _);
+    pub const IDC_ALIAS: PCWSTR = PCWSTR(1 as _);
+    pub const IDC_CELL: PCWSTR = PCWSTR(2 as _);
+    pub const IDC_COLRESIZE: PCWSTR = PCWSTR(3 as _);
+    pub const IDC_COPYCUR: PCWSTR = PCWSTR(4 as _);
+    pub const IDC_HAND_GRAB: PCWSTR = PCWSTR(5 as _);
+    pub const IDC_HAND_GRABBING: PCWSTR = PCWSTR(6 as _);
+    pub const IDC_PAN_EAST: PCWSTR = PCWSTR(7 as _);
+    pub const IDC_PAN_MIDDLE: PCWSTR = PCWSTR(8 as _);
+    pub const IDC_PAN_MIDDLE_HORIZONTAL: PCWSTR = PCWSTR(9 as _);
+    pub const IDC_PAN_MIDDLE_VERTICAL: PCWSTR = PCWSTR(10 as _);
+    pub const IDC_PAN_NORTH: PCWSTR = PCWSTR(11 as _);
+    pub const IDC_PAN_NORTH_EAST: PCWSTR = PCWSTR(12 as _);
+    pub const IDC_PAN_NORTH_WEST: PCWSTR = PCWSTR(13 as _);
+    pub const IDC_PAN_SOUTH: PCWSTR = PCWSTR(14 as _);
+    pub const IDC_PAN_SOUTH_EAST: PCWSTR = PCWSTR(15 as _);
+    pub const IDC_PAN_SOUTH_WEST: PCWSTR = PCWSTR(16 as _);
+    pub const IDC_PAN_WEST: PCWSTR = PCWSTR(17 as _);
+    pub const IDC_ROWRESIZE: PCWSTR = PCWSTR(18 as _);
+    pub const IDC_VERTICALTEXT: PCWSTR = PCWSTR(19 as _);
+    pub const IDC_ZOOMIN: PCWSTR = PCWSTR(20 as _);
+    pub const IDC_ZOOMOUT: PCWSTR = PCWSTR(21 as _);
 }
