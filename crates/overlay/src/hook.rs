98100f193b121a69c4c614605f20b1ed8644df08
storycraft <storycraft@pancake.sh>
2025-05-19 22:53:26 +0900

    feat: use DispatchMessage hook instead WH_GETMESSAGE hook

--- crates/overlay/src/hook.rs
+++ crates/overlay/src/hook.rs
@@ -1,5 +1,6 @@
 mod dx;
 mod opengl;
+mod proc;
 
 pub use dx::util::call_original_execute_command_lists;
 
@@ -18,12 +19,12 @@ use crate::detours::{DetourAttach, DetourTransactionBegin, DetourTransactionComm
 
 #[tracing::instrument]
 pub fn install(dummy_hwnd: HWND) -> anyhow::Result<()> {
+    proc::hook().context("DispatchMessage hook initialization failed")?;
     dx::hook(dummy_hwnd).context("Direct3D hook initialization failed")?;
     opengl::hook(dummy_hwnd).context("OpenGL hook initialization failed")?;
 
     Ok(())
 }
-
 struct DetourHook<F> {
     func: F,
 }
---
+++ crates/overlay/src/hook/proc.rs
@@ -0,0 +1,58 @@
+use once_cell::sync::OnceCell;
+use tracing::{debug, trace};
+use windows::Win32::{Foundation::LRESULT, UI::WindowsAndMessaging::MSG};
+
+use crate::backend::proc::dispatch_message;
+
+use super::DetourHook;
+
+#[link(name = "user32.dll", kind = "raw-dylib", modifiers = "+verbatim")]
+unsafe extern "system" {
+    fn DispatchMessageA(msg: *const MSG) -> LRESULT;
+    fn DispatchMessageW(msg: *const MSG) -> LRESULT;
+}
+
+static HOOK: OnceCell<(DetourHook<DispatchMessageFn>, DetourHook<DispatchMessageFn>)> =
+    OnceCell::new();
+
+type DispatchMessageFn = unsafe extern "system" fn(*const MSG) -> LRESULT;
+
+pub fn hook() -> anyhow::Result<()> {
+    HOOK.get_or_try_init(|| unsafe {
+        debug!("hooking DispatchMessageA");
+        let dispatch_message_a =
+            DetourHook::attach(DispatchMessageA as _, hooked_dispatch_message_a as _)?;
+
+        debug!("hooking DispatchMessageW");
+        let dispatch_message_w =
+            DetourHook::attach(DispatchMessageW as _, hooked_dispatch_message_w as _)?;
+
+        Ok::<_, anyhow::Error>((dispatch_message_a, dispatch_message_w))
+    })?;
+
+    Ok(())
+}
+
+#[tracing::instrument]
+extern "system" fn hooked_dispatch_message_a(msg: *const MSG) -> LRESULT {
+    trace!("DispatchMessageA called");
+
+    if let Some(ret) = dispatch_message(unsafe { &*msg }) {
+        return ret;
+    }
+
+    let hook = HOOK.get().unwrap();
+    unsafe { hook.0.original_fn()(msg) }
+}
+
+#[tracing::instrument]
+extern "system" fn hooked_dispatch_message_w(msg: *const MSG) -> LRESULT {
+    trace!("DispatchMessageW called");
+
+    if let Some(ret) = dispatch_message(unsafe { &*msg }) {
+        return ret;
+    }
+
+    let hook = HOOK.get().unwrap();
+    unsafe { hook.1.original_fn()(msg) }
+}
