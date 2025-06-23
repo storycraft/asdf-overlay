bb291654d27ee1ac8a6d96b2733d3b57ef0ff5f4
storycraft <storycraft@pancake.sh>
2025-06-23 17:50:38 +0900

    chore: include present index

--- crates/overlay/src/vulkan_layer/device/queue.rs
+++ crates/overlay/src/vulkan_layer/device/queue.rs
@@ -17,11 +17,15 @@ pub extern "system" fn present(queue: vk::Queue, info: *const vk::PresentInfoKHR
     if OverlayIpc::connected() {
         unsafe {
             let info = &*info;
-            for &swapchain in slice::from_raw_parts(info.p_swapchains, info.swapchain_count as _) {
+            let swapchains = slice::from_raw_parts(info.p_swapchains, info.swapchain_count as _);
+            let indices = slice::from_raw_parts(info.p_image_indices, info.swapchain_count as _);
+            for i in 0..info.swapchain_count as usize {
+                let swapchain = swapchains[i];
+                let index = indices[i];
                 let data = get_swapchain_data(swapchain);
 
                 if let Err(err) = Backends::with_or_init_backend(HWND(data.hwnd as _), |backend| {
-                    draw_overlay(data.device, queue, backend)
+                    draw_overlay(data.device, queue, index, backend)
                 }) {
                     error!("Backends::with_or_init_backend failed. err: {err:?}");
                 }
@@ -32,7 +36,7 @@ pub extern "system" fn present(queue: vk::Queue, info: *const vk::PresentInfoKHR
     unsafe { (DISPATCH_TABLE.get(&device.as_raw()).unwrap().queue_present)(queue, info) }
 }
 
-fn draw_overlay(device: vk::Device, queue: vk::Queue, backend: &mut WindowBackend) {
+fn draw_overlay(device: vk::Device, queue: vk::Queue, index: u32, backend: &mut WindowBackend) {
     let renderer = match backend.renderer {
         Some(Renderer::Vulkan(ref mut renderer)) => renderer,
         Some(_) => {
@@ -46,6 +50,4 @@ fn draw_overlay(device: vk::Device, queue: vk::Queue, backend: &mut WindowBacken
             return;
         }
     };
-
-    
 }
