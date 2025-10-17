82d4fbd8f94b3773875f5b5ab109bc1d6c5b71c6
storycraft <storycraft@pancake.sh>
2025-10-17 16:44:20 +0900

    chore: remove old build artifacts

--- lib/util.d.ts
+++
@@ -1,4 +0,0 @@
-import { Key, PercentLength } from './index.js';
-export declare function percent(value: number): PercentLength;
-export declare function length(value: number): PercentLength;
-export declare function key(code: number, extended?: boolean): Key;
