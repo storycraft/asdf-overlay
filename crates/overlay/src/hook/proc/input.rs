5e1b3696b28450d0a1b32e907c0ebcab55e1e3ac
storycraft <storycraft@pancake.sh>
2025-10-30 17:21:10 +0900

    chore: fmt, clippy

--- crates/overlay/src/hook/proc/input.rs
+++ crates/overlay/src/hook/proc/input.rs
@@ -100,8 +100,7 @@ pub fn hook() -> anyhow::Result<()> {
         let get_clip_cursor = DetourHook::attach(GetClipCursor as _, hooked_get_clip_cursor as _)?;
 
         debug!("hooking GetCursorPos");
-        let get_cursor_pos =
-            DetourHook::attach(GetCursorPos as _, hooked_get_cursor_pos as _)?;
+        let get_cursor_pos = DetourHook::attach(GetCursorPos as _, hooked_get_cursor_pos as _)?;
 
         debug!("hooking GetPhysicalCursorPos");
         let get_physical_cursor_pos = DetourHook::attach(
