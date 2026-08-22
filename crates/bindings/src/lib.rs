db0dee7f573165ea3b7e9e984d8e8269af934d99
storycraft <storycraft@pancake.sh>
2026-08-22 12:59:12 +0900

    chore: remove unused runtime

--- crates/bindings/src/lib.rs
+++ crates/bindings/src/lib.rs
@@ -7,16 +7,5 @@ pub mod types;
 #[cfg(feature = "window")]
 pub mod window;
 
-use std::sync::LazyLock;
-
-use tokio::runtime::{self, Runtime};
-
-static RUNTIME: LazyLock<Runtime> = LazyLock::new(|| {
-    runtime::Builder::new_multi_thread()
-        .enable_all()
-        .build()
-        .expect("Failed to create Tokio runtime")
-});
-
 #[cfg(feature = "uniffi")]
 uniffi::setup_scaffolding!();
