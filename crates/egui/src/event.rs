1686ff0096f9d8627707dbdb48d5c3471d458c14
storycraft <me@pancake.sh>
2026-09-19 21:32:47 +0900

    feat: add Debug impl to Event

--- crates/egui/src/event.rs
+++ crates/egui/src/event.rs
@@ -1,6 +1,6 @@
 use egui::RequestRepaintInfo;
 
-#[derive(derive_more::From)]
+#[derive(Debug, derive_more::From)]
 pub enum Event {
     Overlay(#[from] asdf_overlay_event::Event),
     Window(#[from] asdf_overlay_window_event::Event),
